72a107b874f0e75451bacc0ed33add465ef22231
lar9482 <lar9482@utulsa.edu>
2024-02-23 12:30:40 -0600

    Completed problem 6

--- problem#6/fib.py
+++ problem#6/fib.py
@@ -4,8 +4,13 @@
 """
 
 def fibonacci(n):
-    #write your code here
-    pass
+    nums = []
+    nums.append(0)
+    nums.append(1)
+    for i in range(2, n+1):
+        nums.append(nums[i-1] + nums[i-2])
+    
+    return nums[n]
 
 if __name__ == '__main__':
     start_num = int(input())
